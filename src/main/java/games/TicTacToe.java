fafcbcd0ead09712c7be5083b569aa6dbe20ab6e
thebellywolf <cdspenzer@gmail.com>
2023-10-11 17:54:08 -0400

    added tic tac toe notes

--- src/main/java/games/TicTacToe.java
+++ src/main/java/games/TicTacToe.java
@@ -11,7 +11,30 @@ public class TicTacToe extends Game{
     //Think about how to keep track of where those xs and os are
     //Probably a counter, if 3 you win---> counter only goes up based on three in row on board
 
-    
+    // Need to keep track of assigning or picking X's or O's
+    // As well as crossing off possibly spots as they are taken
+
+    //There are nine possible spots in Tac Tac Toe
+    // && 8 possible winning combos to keep track of (use booleans)
+
+    //              |               |
+    //    Top Left  |   Top Middle  | Top Right
+    // --------------------------------------------
+    //              |               |
+    //  Middle Left | Middle Middle | Middle Right
+    // --------------------------------------------
+    //  Bottom Left | Bottom Middle | Bottom Right
+    //              |               |
+
+    // if Top Left && Middle Middle && Bottom Right == True
+    // if Top Middle && Middle Middle && Bottom Middle == True
+    // if Bottom Left && Middle Middle && Top Right == True
+    // if Middle Left && Middle Middle && Middle Right == True
+
+    // if Top Left && Middle Left && Bottom Left == True
+    // if Top Left && Top Middle && Top Right == true
+    // if Top Right && Middle Right && Bottom Right == true
+    // if Bottom Left && Bottom Middle && Bottom Right == true
 
 
 
