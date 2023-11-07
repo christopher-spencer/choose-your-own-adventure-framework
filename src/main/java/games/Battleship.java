256ab733366a958937378f9e6f38aa87022ad28c
thebellywolf <cdspenzer@gmail.com>
2023-11-07 14:14:57 -0500

    battleship notes

--- src/main/java/games/Battleship.java
+++ src/main/java/games/Battleship.java
@@ -3,4 +3,10 @@ package games;
 public class Battleship {
 
     // Something similar to TicTacToe where need to figure out CLI graph will work
+
+    // Keep track of every single battleship location
+    // Keep track of misses vs hits
+    // If right number of hits, sink ships
+    
+    // Player wins when all opponent's ships have sunk
 }
