dcd957b583b1725a497ac61ade5e744810a63b6e
thebellywolf <cdspenzer@gmail.com>
2023-11-08 17:55:53 -0500

    Hangman note

--- src/main/java/games/Hangman.java
+++ src/main/java/games/Hangman.java
@@ -6,6 +6,9 @@ import java.util.Random;
 
 public class Hangman extends Game {
 
+    //todo switch things up to ensure you're using HM class more fully & improve OOP
+
+
     private List<String> mysteryWords;
     private String guessedLetter;
     private List<Character> correctGuesses = new ArrayList<>();
