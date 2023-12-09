6c23b4eca139162a085b5add6f35bff668233c4e
thebellywolf <cdspenzer@gmail.com>
2023-12-09 15:33:02 -0500

    removed outdated commented out ttt graph

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -302,11 +302,6 @@ public class Menu {
         System.out.println();
         printTicTacToeBoard();
         System.out.println();
-//        System.out.println("    (1) | (2) | (3)");
-//        System.out.println("    ----------------");
-//        System.out.println("    (4) | (5) | (6)");
-//        System.out.println("    ----------------");
-//        System.out.println("    (7) | (8) | (9)");
         return in.nextLine();
     }
 
