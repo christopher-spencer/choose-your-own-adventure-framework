5b58ba1d6fe6c5d52367f4830f376a1ab4493916
thebellywolf <cdspenzer@gmail.com>
2023-12-15 14:45:10 -0500

    adding if player vs postman win print lines to TTTGame

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -292,6 +292,8 @@ public class Menu {
             if (!isNoMoreMovesPossible) {
                 postmanTicTacToeMove(spotTaken);
                 ticTacToeMoveLogic(spotTaken);
+            } else {
+                isATie = true;
             }
 
             movesLeft = ticTacToeGame.calculateNumberOfMovesTillGameOver(movesLeft);
@@ -302,6 +304,16 @@ public class Menu {
             } else if (isNoMoreMovesPossible) {
                 isATie = true;
             }
+        }
+
+        if (isPlayerWin) {
+            System.out.println("    You win!");
+            System.out.println();
+            System.out.println("    The postman goes to the kitchen and picks up all of your cereal boxes one by one. He dumps them in a mop bucket, then fills the bucket with whole milk. He kicks it over.");
+        } else if (isPostmanWin) {
+            System.out.println("    The postman wins!");
+            System.out.println();
+            System.out.println("The postman pulls a whoopie cushion out of his satchel, blows it up, then squishes it against your forehead.");
         }
     }
 
