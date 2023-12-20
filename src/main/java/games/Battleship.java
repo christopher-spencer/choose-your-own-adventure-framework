9da96c2459cf54324cfda9bbe97caca7e483f8a8
thebellywolf <cdspenzer@gmail.com>
2023-12-20 10:28:55 -0500

    added some notes and variables to BattleShip

--- src/main/java/games/Battleship.java
+++ src/main/java/games/Battleship.java
@@ -2,6 +2,12 @@ package games;
 
 public class Battleship {
 
+    int carrier = 5;
+    int battleShip = 4;
+    int destroyer = 3;
+    int submarine = 3;
+    int patrolBoat = 2;
+    
     // Something similar to TicTacToe where need to figure out CLI graph will work
 
     // Keep track of every single battleship location
@@ -9,4 +15,6 @@ public class Battleship {
     // If right number of hits, sink ships
 
     // Player wins when all opponent's ships have sunk
+
+    // Need a 2D Array for PlayerBoard and PostmanBoard, but only print PlayerBoard
 }
