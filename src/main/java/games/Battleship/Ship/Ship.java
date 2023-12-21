cd3117420914b0b9bda87e951f21bff3a3553ddd
thebellywolf <cdspenzer@gmail.com>
2023-12-21 12:01:56 -0500

    put together a possible Ship object class to use in Battleship

--- src/main/java/games/Battleship/Ship/Ship.java
+++ src/main/java/games/Battleship/Ship/Ship.java
@@ -1,4 +1,39 @@
 package games.Battleship.Ship;
 
 public class Ship {
+
+    private String name;
+    private int size;
+    private int hitPoints;
+    private boolean isSunk;
+
+    public Ship(String name, int size) {
+        this.name = name;
+        this.size = size;
+        this.hitPoints = size;
+        this.isSunk = false;
+    }
+
+    public void hit() {
+        hitPoints--;
+        if (hitPoints == 0) {
+            isSunk = true;
+        }
+    }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public int getSize() { return size; }
+
+    public void setSize(int size) { this.size = size; }
+
+    public int getHitPoints() { return hitPoints; }
+
+    public void setHitPoints(int hitPoints) { this.hitPoints = hitPoints; }
+
+    public boolean isSunk() { return isSunk; }
+
+    public void setSunk(boolean sunk) { isSunk = sunk; }
 }
