e4925d05cf1b99e495552dbd04984aea8df5fea5
thebellywolf <cdspenzer@gmail.com>
2024-01-03 14:39:35 -0500

    uncommented postmanMove method in battleship class

--- src/main/java/games/Battleship/Battleship.java
+++ src/main/java/games/Battleship/Battleship.java
@@ -102,16 +102,16 @@ public class Battleship extends Game {
         }
     }
 
-//        public void postmanMove(String[][] playerBoard, String[][] postmanOpponentDisplay) {
-//        // TODO Update playerBoard and postmanOpponentDisplay
-//        Random random = new Random();
-//        int row = random.nextInt(9) + 1;
-//        int col = random.nextInt(9) + 1;
-//
-//        String result = attack(playerBoard, row, col);
-//        postmanOpponentDisplay[row][col] = result;
-//        // Additional logic for checking if a ship is sunk (?)
-//    }
+        public void postmanMove(String[][] playerBoard, String[][] postmanOpponentDisplay) {
+        // TODO Update playerBoard and postmanOpponentDisplay
+        Random random = new Random();
+        int row = random.nextInt(9) + 1;
+        int col = random.nextInt(9) + 1;
+
+        String result = attack(playerBoard, row, col);
+        postmanOpponentDisplay[row][col] = result;
+        // Additional logic for checking if a ship is sunk (?)
+    }
 
     public boolean isPlacementValid(Ship ship, String[][] board) {
         int startRow = ship.getStartRow();
