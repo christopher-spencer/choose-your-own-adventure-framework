2c65321bd73f37e6385393e0d188ba2ab7b4e1cd
thebellywolf <cdspenzer@gmail.com>
2024-01-09 11:31:54 -0500

    updated Hangman todo

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -248,6 +248,8 @@ public class Menu {
 
         //TODO on final word it doesn't fill in the last space, just tells you the word,
         // but last space would be satisfying
+        // and Maybe the YOU WIN needs to stick out more,
+        // or don't need that final "You guessed a correct letter!" if YOU WIN (?)
 
         welcomeToHangman();
 
