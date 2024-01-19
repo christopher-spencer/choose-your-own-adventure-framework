cd508935c012607d45b2e7c6229f83e30f4d9026
thebellywolf <cdspenzer@gmail.com>
2024-01-19 13:40:45 -0500

    deleted TTT TODO

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -628,12 +628,6 @@ public class Menu {
 
     // TODO check the array for isAWinner logic and so on instead of the booleans
 
-    // TODO issue where game says its a tie before postman made its final winning move
-    //  (Make sure that the check for a winner (isAWinner()) and a tie (isATie()) is done
-    //  after both the player and the postman have made their moves in each cycle of the game
-    //  loop. If the check for a tie is happening before the postman's move, it might
-    //  incorrectly conclude the game as a tie.)
-
     private void ticTacToeGame() {
 
         List<Integer> spotTaken = ticTacToeGame.getSpotTaken();
