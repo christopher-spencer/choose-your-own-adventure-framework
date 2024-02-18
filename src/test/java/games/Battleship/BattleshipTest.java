9ec1022d4120605af05736d051aa3cefafbaf65a
thebellywolf <cdspenzer@gmail.com>
2024-02-18 09:50:11 -0500

    added BattleshipTest.java

---
+++ src/test/java/games/Battleship/BattleshipTest.java
@@ -0,0 +1,66 @@
+package games.Battleship;
+
+import static org.junit.Assert.*;
+
+public class BattleshipTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void isShipSunk() {
+    }
+
+    @org.junit.Test
+    public void isAWinner() {
+    }
+
+    @org.junit.Test
+    public void isPlayerWin() {
+    }
+
+    @org.junit.Test
+    public void isPostmanWin() {
+    }
+
+    @org.junit.Test
+    public void resetGame() {
+    }
+
+    @org.junit.Test
+    public void getShipByOwnerAndShipName() {
+    }
+
+    @org.junit.Test
+    public void isPlacementValid() {
+    }
+
+    @org.junit.Test
+    public void getShipCoordinate() {
+    }
+
+    @org.junit.Test
+    public void updatePlayerShipLocation() {
+    }
+
+    @org.junit.Test
+    public void updatePostmanShipLocation() {
+    }
+
+    @org.junit.Test
+    public void playerMove() {
+    }
+
+    @org.junit.Test
+    public void postmanMove() {
+    }
+
+    @org.junit.Test
+    public void isSpotAlreadyAttacked() {
+    }
+}
